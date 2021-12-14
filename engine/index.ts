981d170f6d1cd3f36c7e7d3cf71bd71a5abbac4f
CorentinFaget <corentin.faget@quantsunited.com>
2021-12-14 09:41:50 +0100

    Test prompt interaction

--- engine/index.ts
+++ engine/index.ts
@@ -1,5 +1,51 @@
-function hello(name: string) {
-    console.log(`Hello ${name}`);
+import inquirer from "inquirer";
+
+
+async function start301(){
+    await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'status',
+                message: 'Which game do you want to play ?',
+                choices: ['start', 'cancel']
+            },
+        ])
+        .then(answers => {
+            switch(answers.status){
+                case 'start':
+                    console.info(`The game is started`);
+                    break;
+                case 'cancel':
+                    startGame();
+                    break;
+            }
+        });
 }
 
-hello("world");
+async function startGame(){
+    await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'game',
+                message: 'Which game do you want to play ?',
+                choices: ['Tour du Monde', '301', 'Cricket']
+            },
+        ])
+        .then(answers => {
+            switch(answers.game){
+                case 'Tour du Monde':
+                    console.info(`You start Tour du Monde.`);
+                    break;
+                case '301':
+                    start301();
+                    break;
+                case 'Cricket':
+                    console.info(`You start Cricket.`);
+                    break;
+            }
+        });
+}
+
+startGame()
